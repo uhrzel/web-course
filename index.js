f83c03346ee539dd51aeac2661380d3fedce284d
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-07-11 20:17:34 +0800

    calculator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const demo = document.querySelector("#demo");
+/* const demo = document.querySelector("#demo");
 const length = demo.textContent.length;
 let solve = "";
 for (let x = 0; x < length; x++) {
@@ -8,3 +8,4 @@ document.body.append(solve);
 
 let x = { arzel: "arzel", john: "john" };
 console.log(typeof x);
+ */
